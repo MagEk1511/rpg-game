8d277a789cb2edeb5748c7b3605df53bffadab6b
MagEk1511 <ekamagerramov@gmail.com>
2025-12-16 21:49:36 +0300

    Added webpack and base classes

---
+++ src/js/game.js
@@ -0,0 +1,10 @@
+import Player from './characters/Player';
+import Archer from './characters/Archer';
+import Warrior from './characters/Warrior';
+import Mage from './characters/Mage';
+import Dwart from './characters/Dwart';
+import Crossbowman from './characters/Crossbowman';
+import Demourge from './characters/Demourge';
+
+export function play() {
+}
